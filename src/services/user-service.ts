b5880a54ee060073abb8643ad20b925c471f3e74
perryc85 <cjperry85@outlook.com>
2020-02-25 21:09:35 -0500

    added update function for user patch

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,15 +1,22 @@
-import { daoFindAllUsers, daoFindUserByUsernameAndPassword, daoFindUserById } from '../repositories/user-dao';
+import { daoFindAllUsers, daoFindUserByUsernameAndPassword, daoFindUserById, daoUpdateOneUser } from '../repositories/user-dao';
 import { User } from '../models/User';
+import { UserDTO } from '../dtos/UserDTO';
  
-//  daoFindAllUsers
+// dao Find All Users
  export async function findAllUsers():Promise<User[]>{
     return await daoFindAllUsers()
  }
 
+ // dao Find user by username and password
  export async function findUserByUsernameAndPassword(username:string, password:string): Promise<User>{
    return await daoFindUserByUsernameAndPassword(username,password)
 }
 
+// dao finding user by id
 export async function findUserById(id:number):Promise<User>{
    return await daoFindUserById(id)
+}
+
+export async function updateOneUser(newUser:UserDTO):Promise<User>{
+   return await daoUpdateOneUser(newUser)
 }
