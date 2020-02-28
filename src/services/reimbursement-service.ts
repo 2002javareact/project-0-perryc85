a261d675c77c4969499c89acbef010cacd3ad147
perryc85 <cjperry85@outlook.com>
2020-02-27 22:42:22 -0500

    added create function and imported the dao function

--- src/services/reimbursement-service.ts
+++ src/services/reimbursement-service.ts
@@ -1,5 +1,6 @@
 import { Reimbursement } from "../models/Reimbursement";
-import { daoFindAllReimbursements, daoFindReimbursementByStatus, daoFindReimbursementByUser } from "../repositories/reimbursement-dao";
+import { daoFindAllReimbursements, daoFindReimbursementByStatus, daoFindReimbursementByUser, daoCreateReimbursement } from "../repositories/reimbursement-dao";
+import { ReimbursementDTO } from "../dtos/ReimbursementDTO";
 
 // dao Find All Users
 export async function findAllReimbursements():Promise<Reimbursement[]>{
@@ -12,4 +13,8 @@ export async function findAllReimbursements():Promise<Reimbursement[]>{
 
  export async function findReimbursementByUser(id:number): Promise<Reimbursement[]>{
     return await daoFindReimbursementByUser(id)
+ }
+
+ export async function createReimbursement(newSubmission:ReimbursementDTO):Promise<Reimbursement[]>{
+   return await daoCreateReimbursement(newSubmission)
  }
